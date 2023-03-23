f7f8dc1970005017c10187ec3e85b4286829d144
DuyHuynh <duyhuynh61@gmail.com>
2023-03-23 18:06:18 +0700

    aa

--- sync_data.bat
+++ sync_data.bat
@@ -1,3 +1,1 @@
-@echo off
-echo "ngocdung" | git add . && git commit -m "aa" && git push origin main
-msra /offerra
+git add . && git commit -m "aa" && git push origin main
--- sync_data_main.bat
+++
@@ -1,1 +0,0 @@
-start sync_data.bat < echo 'ngocdung'
